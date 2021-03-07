95c22d63f618a3317bf9863cbac21d424c6daf0c
odusseus <info@odusseus.org>
2021-03-07 18:15:03 +0100

    Unittest for bishop added.

--- mat/Bishop.py
+++ mat/Bishop.py
@@ -3,7 +3,7 @@ from Piece import Piece
 
 
 class Bishop(Piece):
-    def __init__(self, colorId, pieceCode, fieldId=None, value=None):
+    def __init__(self, colorId, pieceCode, value=None, fieldId=None):
         Piece.__init__(
             self,
             Constant.BISHOP,
@@ -15,5 +15,9 @@ class Bishop(Piece):
         )
 
     def clone(self):
-        clone = Bishop(self.color, self.code, self.value, self.field.id)
+        id = None
+        if self.field != None:
+          id = self.field.id
+
+        clone = Bishop(self.color.id, self.code, self.value, id)
         return clone
--- mattest/Bishop_test.py
+++ mattest/Bishop_test.py
@@ -1,6 +1,7 @@
 import Config
 import Constant
 from Bishop import Bishop
+from Color import Color
 
 Config.Init()
 
@@ -12,10 +13,21 @@ def test_Bishop_return_a_Bishop():
 
 def test_Bishop_with_field_return_a_Bishop_on_a_Field():
   Config.Init()
-  bishop = Bishop(Constant.BLACK, Constant.BISHOPBLACKCODE, 1)
+  bishop = Bishop(Constant.BLACK, Constant.BISHOPBLACKCODE, 3, 1)
   assert bishop.color.id == Constant.BLACK
   assert bishop.color.name == Constant.BLACKNAME
   assert bishop.field.id == 1
   assert bishop.field.color.name == Constant.BLACKNAME
   assert bishop.field.columnName == 'a'
   assert bishop.field.lineName == 1
+
+def test_clone():
+  Config.Init()
+  bishop = Bishop(Constant.BLACK, Constant.BISHOPBLACKCODE, 3, 1)
+  
+  clone = bishop.clone()
+  assert clone.color.id == bishop.color.id
+  assert clone.code == bishop.code
+  assert clone.name == bishop.name
+  assert clone.value == bishop.value
+  assert clone.field.id == bishop.field.id
